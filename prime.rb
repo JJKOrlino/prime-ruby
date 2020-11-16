86f6a1d48248bfbe4f928844c35f9fb78cae74bf
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-16 15:04:27 -0600

    commit tree 071dad2715203a1dce957edac510e1e9d49e8d96

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(array)
+    # i = 0
+    #if array < 1
+    if array < 2 #{|i| i.prime}
+        return false
+    end
+    #i = 1
+    i = 2
+        while i <= array / 2
+            if array % i == 0
+            return false
+        end
+        i += 1
+    end
+    return true
+end
